fd14ee83f183513aae761d7dc425f0d655fe82d0
Eagle-44 <alexandrofutbolist@gmail.com>
2023-06-15 02:15:17 +0300

    added fetching data on the web page and reflected them

--- complex_lab_backend/src/main/resources/static/js/admin.js
+++ complex_lab_backend/src/main/resources/static/js/admin.js
@@ -1,51 +1,97 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const catalogSelect = document.getElementById("catalogSelect");
+    const catalogTable = document.getElementById("catalogTable");
     const addBookButton = document.getElementById("addBookButton");
     const deleteBookButton = document.getElementById("deleteBookButton");
     const monthlyReportButton = document.getElementById("monthlyReportButton");
     const returnBookButton = document.getElementById("returnBookButton");
-    const catalogSelect = document.getElementById("catalogSelect");
-    const catalogTable = document.getElementById("catalogTable");
-  
-    // Add book button click event handler
-    addBookButton.addEventListener("click", function() {
-      // Logic for adding a book
-    });
-  
-    // Delete book button click event handler
-    deleteBookButton.addEventListener("click", function() {
-      // Logic for deleting a book
-    });
-  
+
+
+    // // Add book button click event handler
+    // addBookButton.addEventListener("click", function() {
+    //   // Logic for adding a book
+    // });
+    //
+    // // Delete book button click event handler
+    // deleteBookButton.addEventListener("click", function() {
+    //   // Logic for deleting a book
+    // });
+
     // Monthly report button click event handler
     monthlyReportButton.addEventListener("click", function() {
-      
+
       // Redirect to the monthly report page
       window.location.href = "monthly_report.html";
     });
 
-    
+
     returnBookButton.addEventListener("click", function() {
-      
+
       // Redirect to the return book page
       window.location.href = "return_book_page.html";
     });
-  
-    // Catalog select change event handler
-    catalogSelect.addEventListener("change", function() {
-      const selectedOption = catalogSelect.value;
-  
-      // Logic for updating the table based on selected option
-      if (selectedOption === "users") {
-        // Display table with list of users
-        catalogTable.innerHTML = "<thead><tr><th>User ID</th><th>Name</th></tr></thead><tbody><tr><td>1</td><td>John Doe</td></tr><tr><td>2</td><td>Jane Smith</td></tr></tbody>";
-      } else if (selectedOption === "rentedBooks") {
-        // Display table with users who have rented books
-        catalogTable.innerHTML = "<thead><tr><th>User ID</th><th>Name</th><th>Rented Books</th></tr></thead><tbody><tr><td>1</td><td>John Doe</td><td>Book 1, Book 2</td></tr><tr><td>2</td><td>Jane Smith</td><td>Book 3</td></tr></tbody>";
-      } else if (selectedOption === "overdueBooks") {
-        // Display table with users who have overdue books
-        catalogTable.innerHTML = "<thead><tr><th>User ID</th><th>Name</th><th>Overdue Books</th></tr></thead><tbody><tr><td>1</td><td>John Doe</td><td>Book 1</td></tr><tr><td>2</td><td>Jane Smith</td><td>Book 3</td></tr></tbody>";
-      }
-    });
 
-  });
-  
+    // Catalog select change event handler
+        catalogSelect.addEventListener("change", function() {
+            const selectedOption = catalogSelect.value;
+
+            // Logic for updating the table based on selected option
+            if (selectedOption === "users") {
+                fetch("http://localhost:8080/users")
+                    .then(response => response.json())
+                    .then(users => {
+                        renderUsersTable(users);
+                    })
+                    .catch(error => {
+                        console.error("Error fetching users:", error);
+                    });
+            } else if (selectedOption === "rentedBooks") {
+                Promise.all([fetch("http://localhost:8080/loans"), fetch("http://localhost:8080/users")])
+                    .then(([loansResponse, usersResponse]) => {
+                        return Promise.all([loansResponse.json(), usersResponse.json()]);
+                    })
+                    .then(([loans, users]) => {
+                        renderRentedBooksTable(loans, users);
+                    })
+                    .catch(error => {
+                        console.error("Error fetching rented books and users:", error);
+                    });
+            }
+        });
+
+        // Function to render the users table
+        function renderUsersTable(users) {
+            catalogTable.innerHTML = `
+      <thead>
+        <tr>
+          <th>User ID</th>
+          <th>Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${users.map(user => `<tr><td>${user.id}</td><td>${user.fullName}</td></tr>`).join("")}
+      </tbody>
+    `;
+        }
+
+        // Function to render the rented books table
+        function renderRentedBooksTable(loans, users) {
+            catalogTable.innerHTML = `
+      <thead>
+        <tr>
+          <th>User ID</th>
+          <th>Name</th>
+          <th>Rented Books ID</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${loans.map(loan => {
+                const user = users.find(user => user.id === loan.userId);
+                return `<tr><td>${loan.userId}</td><td>${user.fullName}</td><td>${loan.bookId}</td></tr>`;
+            }).join("")}
+      </tbody>
+    `;
+        }
+});
+
+
--- complex_lab_backend/src/main/resources/static/js/returnBook.js
+++ complex_lab_backend/src/main/resources/static/js/returnBook.js
@@ -1,74 +1,46 @@
 document.addEventListener("DOMContentLoaded", function() {
     const bookSelect = document.getElementById("bookSelect");
-    const rentalCost = document.getElementById("rentalCost");
-    const damageFine = document.getElementById("damageFine");
-    const totalCost = document.getElementById("totalCost");
+    const damageFineInput = document.getElementById("damageFine");
     const confirmButton = document.getElementById("confirmButton");
-  
-    // Fetch the book data from the backend and populate the book select dropdown
-    fetch("my-backend-api-endpoint")
-      .then(response => response.json())
-      .then(data => {
-        // Populate the book select dropdown with book options
-        data.forEach(book => {
-          const option = document.createElement("option");
-          option.value = book.id;
-          option.textContent = book.title;
-          bookSelect.appendChild(option);
-        });
-      })
-      .catch(error => {
-        console.error("Error fetching book data:", error);
-      });
-  
-    // Calculate the total cost when the rental cost or damage fine value changes
-    function calculateTotalCost() {
-      const selectedBookId = bookSelect.value;
-      const selectedRentalCost = parseFloat(rentalCost.textContent);
-      const selectedDamageFine = parseFloat(damageFine.value);
-  
-      // Call the backend endpoint to calculate the total cost
-      fetch(`my-backend-api-endpoint?bookId=${selectedBookId}&rentalCost=${selectedRentalCost}&damageFine=${selectedDamageFine}`)
+
+    // Load books into the select dropdown
+    fetch("http://localhost:8080/books")
         .then(response => response.json())
-        .then(data => {
-          totalCost.textContent = data.totalCost;
+        .then(books => {
+            books.forEach(book => {
+                if (book.isRented) {
+                    const option = document.createElement("option");
+                    option.value = book.id;
+                    option.textContent = book.name;
+                    bookSelect.appendChild(option);
+                }
+            });
         })
         .catch(error => {
-          console.error("Error calculating total cost:", error);
+            console.error("Error fetching books:", error);
         });
-    }
-  
-    // Event listeners for value changes
-    bookSelect.addEventListener("change", calculateTotalCost);
-    damageFine.addEventListener("input", calculateTotalCost);
-  
-    // Event listener for the confirm button click
+
+    // Handle confirm button click event
     confirmButton.addEventListener("click", function() {
-      const selectedBookId = bookSelect.value;
-      const selectedDamageFine = parseFloat(damageFine.value);
-      const selectedTotalCost = parseFloat(totalCost.textContent);
-  
-      // Call the backend endpoint to process the return and handle the payment
-      // Replace the URL with your actual backend endpoint
-      fetch("my-backend-api-endpoint", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          bookId: selectedBookId,
-          damageFine: selectedDamageFine,
-          totalCost: selectedTotalCost
+        const selectedBookId = bookSelect.value;
+        const damageFine = parseFloat(damageFineInput.value) || 0;
+
+        // Send return book request to the server
+        fetch(`http://localhost:8080/loans/${selectedBookId}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ damageFine })
         })
-      })
-        .then(response => response.json())
-        .then(data => {
-          // Handle the response from the backend
-          console.log("Return processed:", data);
-        })
-        .catch(error => {
-          console.error("Error processing return:", error);
-        });
+            .then(response => response.json())
+            .then(data => {
+                console.log("Book returned successfully:", data);
+                // Handle success response, e.g., show a success message to the user
+            })
+            .catch(error => {
+                console.error("Error returning book:", error);
+                // Handle error response, e.g., show an error message to the user
+            });
     });
-  });
-  
+});
